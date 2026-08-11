82ba40923bd5557202933996c0e276d361c457a1
ValemiMilk <nicolasimoes@valemilk.com.br>
2026-08-11 15:58:50 -0300

    Exportar Usuario do models

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,4 +1,5 @@
 export { Analise, type IAnaliseDocument } from "./Analise";
 export { Padrao, type IPadraoDocument } from "./Padrao";
 export { Produto, type IProdutoDocument } from "./Produto";
+export { Usuario, type IUsuarioDocument } from "./Usuario";
 export { AuditoriaLog, type IAuditoriaLogDocument } from "./AuditoriaLog";
